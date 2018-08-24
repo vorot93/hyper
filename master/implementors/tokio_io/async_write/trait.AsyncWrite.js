a9e2703b81887157153065e57c7372a0f587b396
Sean McArthur <sean.monstar@gmail.com>
2018-08-24 23:53:31 +0000

    rebuild pages at 168c7d2155952ba09f781c331fd67593b820af20

--- master/implementors/tokio_io/async_write/trait.AsyncWrite.js
+++ master/implementors/tokio_io/async_write/trait.AsyncWrite.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["hyper"] = [{text:"impl <a class=\"trait\" href=\"https://docs.rs/tokio-io/0.1.7/tokio_io/async_write/trait.AsyncWrite.html\" title=\"trait tokio_io::async_write::AsyncWrite\">AsyncWrite</a> for <a class=\"struct\" href=\"hyper/upgrade/struct.Upgraded.html\" title=\"struct hyper::upgrade::Upgraded\">Upgraded</a>",synthetic:false,types:["hyper::upgrade::Upgraded"]},];
+implementors["hyper"] = [{text:"impl <a class=\"trait\" href=\"https://docs.rs/tokio-io/0.1.8/tokio_io/async_write/trait.AsyncWrite.html\" title=\"trait tokio_io::async_write::AsyncWrite\">AsyncWrite</a> for <a class=\"struct\" href=\"hyper/upgrade/struct.Upgraded.html\" title=\"struct hyper::upgrade::Upgraded\">Upgraded</a>",synthetic:false,types:["hyper::upgrade::Upgraded"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
