97ccb1bea60ec29410316f6d388ee031c19932a1
Sean McArthur <sean.monstar@gmail.com>
2018-11-17 00:41:12 +0000

    rebuild pages at 95446cc338f8055539dd3503c482d649f42a531c

--- master/implementors/std/io/trait.Read.js
+++ master/implementors/std/io/trait.Read.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["hyper"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.Read.html\" title=\"trait std::io::Read\">Read</a> for <a class=\"struct\" href=\"hyper/upgrade/struct.Upgraded.html\" title=\"struct hyper::upgrade::Upgraded\">Upgraded</a>",synthetic:false,types:["hyper::upgrade::Upgraded"]},];
+implementors["hyper"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.Read.html\" title=\"trait std::io::Read\">Read</a> for <a class=\"struct\" href=\"hyper/server/conn/struct.AddrStream.html\" title=\"struct hyper::server::conn::AddrStream\">AddrStream</a>",synthetic:false,types:["hyper::server::tcp::addr_stream::AddrStream"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.Read.html\" title=\"trait std::io::Read\">Read</a> for <a class=\"struct\" href=\"hyper/upgrade/struct.Upgraded.html\" title=\"struct hyper::upgrade::Upgraded\">Upgraded</a>",synthetic:false,types:["hyper::upgrade::Upgraded"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- master/implementors/tokio_io/async_write/trait.AsyncWrite.js
+++ master/implementors/tokio_io/async_write/trait.AsyncWrite.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["hyper"] = [{text:"impl <a class=\"trait\" href=\"https://docs.rs/tokio-io/0.1.10/tokio_io/async_write/trait.AsyncWrite.html\" title=\"trait tokio_io::async_write::AsyncWrite\">AsyncWrite</a> for <a class=\"struct\" href=\"hyper/upgrade/struct.Upgraded.html\" title=\"struct hyper::upgrade::Upgraded\">Upgraded</a>",synthetic:false,types:["hyper::upgrade::Upgraded"]},];
+implementors["hyper"] = [{text:"impl <a class=\"trait\" href=\"https://docs.rs/tokio-io/0.1.10/tokio_io/async_write/trait.AsyncWrite.html\" title=\"trait tokio_io::async_write::AsyncWrite\">AsyncWrite</a> for <a class=\"struct\" href=\"hyper/server/conn/struct.AddrStream.html\" title=\"struct hyper::server::conn::AddrStream\">AddrStream</a>",synthetic:false,types:["hyper::server::tcp::addr_stream::AddrStream"]},{text:"impl <a class=\"trait\" href=\"https://docs.rs/tokio-io/0.1.10/tokio_io/async_write/trait.AsyncWrite.html\" title=\"trait tokio_io::async_write::AsyncWrite\">AsyncWrite</a> for <a class=\"struct\" href=\"hyper/upgrade/struct.Upgraded.html\" title=\"struct hyper::upgrade::Upgraded\">Upgraded</a>",synthetic:false,types:["hyper::upgrade::Upgraded"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
