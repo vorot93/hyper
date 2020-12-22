eea805f99fb8666d4bad3822083389876341c0dc
github-action-benchmark <github@users.noreply.github.com>
2020-12-22 07:50:07 +0000

    add pipeline (cargo) benchmark result for 6c593c292508f31a2986d5ddac3fb23b234a0e2c

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1608623383061,
+  "lastUpdate": 1608623406914,
   "repoUrl": "https://github.com/vorot93/hyper",
   "entries": {
     "connect": [
@@ -931,6 +931,36 @@ window.BENCHMARK_DATA = {
             "name": "hello_world_16",
             "value": 51502,
             "range": "± 5979",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "46450720+bishtpawan@users.noreply.github.com",
+            "name": "Pawan Singh Bisht",
+            "username": "bishtpawan"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6c593c292508f31a2986d5ddac3fb23b234a0e2c",
+          "message": "refactor(error): use `matches!` macro in `Error::is_*` methods (#2367)",
+          "timestamp": "2020-12-21T12:25:47-08:00",
+          "tree_id": "c61537b497a46b23e64414fa1f2c822dadd22fc4",
+          "url": "https://github.com/vorot93/hyper/commit/6c593c292508f31a2986d5ddac3fb23b234a0e2c"
+        },
+        "date": 1608623399385,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "hello_world_16",
+            "value": 66447,
+            "range": "± 12022",
             "unit": "ns/iter"
           }
         ]
