f347c5bb7691cfa878633db9e44ccb65bddfe6bf
github-action-benchmark <github@users.noreply.github.com>
2021-02-23 03:48:26 +0000

    add pipeline (cargo) benchmark result for 4c946af49cc7fbbc6bd4894283a654625c2ea383

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1614052093639,
+  "lastUpdate": 1614052106236,
   "repoUrl": "https://github.com/vorot93/hyper",
   "entries": {
     "connect": [
@@ -2371,6 +2371,36 @@ window.BENCHMARK_DATA = {
             "name": "hello_world_16",
             "value": 65444,
             "range": "± 9093",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "zonyitoo@users.noreply.github.com",
+            "name": "ty",
+            "username": "zonyitoo"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "4c946af49cc7fbbc6bd4894283a654625c2ea383",
+          "message": "feat(server): add `AddrIncoming::from_listener` constructor (#2439)",
+          "timestamp": "2021-02-22T16:18:38-08:00",
+          "tree_id": "895fb85f54da75512a8656c13aeea97236b7fc04",
+          "url": "https://github.com/vorot93/hyper/commit/4c946af49cc7fbbc6bd4894283a654625c2ea383"
+        },
+        "date": 1614052098955,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "hello_world_16",
+            "value": 64937,
+            "range": "± 12666",
             "unit": "ns/iter"
           }
         ]
