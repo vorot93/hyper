eb1870896b79280bdc42e0a9a73c49bf9b171602
github-action-benchmark <github@users.noreply.github.com>
2021-03-11 07:24:09 +0000

    add pipeline (cargo) benchmark result for 297a068454115e8d5426f9051398d851f69235bf

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1615447393113,
+  "lastUpdate": 1615447449417,
   "repoUrl": "https://github.com/vorot93/hyper",
   "entries": {
     "connect": [
@@ -2611,6 +2611,36 @@ window.BENCHMARK_DATA = {
             "name": "hello_world_16",
             "value": 62556,
             "range": "± 14390",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "346512016@qq.com",
+            "name": "kolapapa",
+            "username": "kolapapa"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "297a068454115e8d5426f9051398d851f69235bf",
+          "message": "docs(examples): upgrade tokio version (#2456)",
+          "timestamp": "2021-03-10T10:00:39-08:00",
+          "tree_id": "98004bc3096176316ed6534e6e96eda3d08cb825",
+          "url": "https://github.com/vorot93/hyper/commit/297a068454115e8d5426f9051398d851f69235bf"
+        },
+        "date": 1615447442030,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "hello_world_16",
+            "value": 66901,
+            "range": "± 12856",
             "unit": "ns/iter"
           }
         ]
